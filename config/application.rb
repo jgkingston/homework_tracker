4c1232f3b48e098ceab6693aa1aaf5551101dcc6
Jamie Kingston <jamie.kingston@gmail.com>
2014-10-14 12:57:15 -0400

    Try to fix some asset pipeline issues

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,7 @@ module HomeworkTracker
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.assets.paths << "#{Rails}/vendor/assets/fonts"
-    config.assets.paths << "#{Rails}/vendor/assets/images"
+    config.assets.enabled = true
 
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false
